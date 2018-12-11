99070341366866ba3ad3611d87e63a8108f8f751
Jacob Huber <jacobnhuber@gmail.com>
2018-12-11 12:16:48 -0600

    Simple console application that takes in a name and displays it with other text

---
+++ C2P1/C2P1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // promot user for a name
+            Console.WriteLine("Enter your name: ");
+
+            // store the value in a variable
+            string name = Console.ReadLine();
+
+            // display the message
+            Console.WriteLine($"Welcome to Agile, {name}");
+
+            Console.ReadKey();
+        }
+    }
+}
