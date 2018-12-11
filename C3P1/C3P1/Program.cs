dde37a2ec79ba52510ef72fcffcb7d100ab26120
Jacob Huber <jacobnhuber@gmail.com>
2018-12-11 14:02:26 -0600

    Console application that will store a score and if the score is greater than or equal to 90 then it will post congratulations.

---
+++ C3P1/C3P1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // promt the user for a score
+            Console.WriteLine("Enter student score:");
+
+            // store the score in a variable
+            double score = double.Parse(Console.ReadLine());
+
+            // check and see if the score is 90 or higher
+            if (score >= 90)
+            {
+                Console.WriteLine("Congratulations!");
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using the system");
+            }
+            Console.ReadKey();
+        }
+    }
+}
