ef6938af897b364ca0389c71e83eb58e08b46974
Jacob Huber <jacobnhuber@gmail.com>
2018-12-11 12:28:22 -0600

    Console application that displays how many calories are consumed based on how many candies the user ate.

---
+++ C2P2/C2P2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables that will hold values
+            const int numberOfCandiesInBag = 50;
+            const int servingsPerBag = 25;
+            const int caloriesPerServing = 100;
+
+            // find out how many candies per serving
+            double candiesPerServing = numberOfCandiesInBag / servingsPerBag;
+
+            // find out how many calories per candy
+            double caloriesPerCandy = caloriesPerServing / candiesPerServing;
+
+            // ask user for number of candies he or she ate
+            Console.WriteLine("How many candies did you eat?");
+
+            // get the number from the user
+            int candiesAte = int.Parse(Console.ReadLine());
+
+            // calculate how many calories consumed
+            double caloriesConsumed = candiesAte * caloriesPerCandy;
+
+            // display data
+            Console.WriteLine($"You have consumed {caloriesConsumed} calories.");
+
+            // pause screen
+            Console.ReadKey();
+        }
+    }
+}
