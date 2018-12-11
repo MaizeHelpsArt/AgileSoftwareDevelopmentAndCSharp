684313710eaef5280a23af4b11cdfc9e136cf702
Jacob Huber <jacobnhuber@gmail.com>
2018-12-11 14:16:36 -0600

    Console application that uses an array to store 10 scores that are entered in by the user. An average, min, and max of the socres will then be displayed for the user to view

---
+++ C3P3/C3P3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare an array that can hold 10 int scores
+            int[] scores = new int[10];
+
+            // using a loop to prompt user for input, one score at a time
+            for (int count = 0; count < scores.Length; count++)
+            {
+                Console.WriteLine("Enter a score:");
+                scores[count] = int.Parse(Console.ReadLine());
+            }
+            // Display statistics
+            Console.WriteLine($"Average: {scores.Average()}");
+            Console.WriteLine($"Highest: {scores.Max()}");
+            Console.WriteLine($"Lowest: {scores.Min()}");
+            Console.ReadKey();
+        }
+    }
+}
