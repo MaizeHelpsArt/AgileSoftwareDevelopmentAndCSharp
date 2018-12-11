b6e8971c469181faaa67ecfca4112a8a4e173a21
Jacob Huber <jacobnhuber@gmail.com>
2018-12-11 12:08:44 -0600

    Simple console application that displays a title and text that has been modified

---
+++ C1P1/C1P1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C1P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a blue background for the text
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            // make the text red
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // create a title
+            Console.Title = "Welcome";
+
+            // display the text
+            Console.WriteLine("Welcome to Agile");
+
+            // program pause waiting for user to hit a key
+            Console.ReadKey();
+        }
+    }
+}
