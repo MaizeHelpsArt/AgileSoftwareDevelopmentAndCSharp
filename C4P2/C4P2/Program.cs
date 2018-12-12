4ae7b9dbfc89305b59ba3b17ae261dcae2359791
Jacob Huber <jacobnhuber@gmail.com>
2018-12-12 09:28:41 -0600

    Console application with a method that will take two values, add them, and then display them.

---
+++ C4P2/C4P2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // two int varriables with values
+            int a = 3;
+            int b = 5;
+
+            // call the Plus() method and store the return value in total
+            int total = Plus(a, b);
+
+            // display result
+            Console.WriteLine($"The sum of {a} and {b} is {total}");
+            Console.ReadKey();
+        }
+
+        // Plus method
+        static int Plus(int x, int y)
+        {
+            int sum;
+            sum = x + y;
+            return sum;
+        }
+    }
+}
