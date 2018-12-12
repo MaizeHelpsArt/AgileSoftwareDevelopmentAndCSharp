76a6ed7a2e89b5a903b7e877f580ad314153630f
Jacob Huber <jacobnhuber@gmail.com>
2018-12-12 09:19:27 -0600

    Console program that displays a method two times with a parameter.

---
+++ C4P1/C4P1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayText("I am number one");
+            DisplayText("I am number two");
+            Console.ReadKey();
+        }
+        // DisplayText method
+        static void DisplayText(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.WriteLine();
+            Console.WriteLine("************");
+        }
+    }
+}
