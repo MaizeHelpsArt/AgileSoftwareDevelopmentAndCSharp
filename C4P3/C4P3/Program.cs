9a7238028b225a3f3d2f9f1ce75b81bfa0b1f897
Jacob Huber <jacobnhuber@gmail.com>
2018-12-12 10:00:49 -0600

    Console application that will take two values, divide them and then display the quotient and remainder.

---
+++ C4P3/C4P3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create three int variables
+            int a = 11;
+            int b = 5;
+            int remainder;
+
+            // call the Divide() method and store the return value in quotient
+            int quotient = Divide(a, b, out remainder);
+
+            // display results
+            Console.WriteLine($"{a} divided by {b} is {quotient} with remainder of {remainder}");
+            Console.ReadKey();
+        }
+        static int Divide(int x, int y, out int r)
+        {
+            int q;
+            q = x / y;
+            r = x % y;
+            return q;
+        }
+    }
+}
