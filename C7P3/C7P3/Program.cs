1782cef7dc62502e3dac2900527feaf8d5a85300
Jacob Huber <jacobnhuber@gmail.com>
2018-12-14 07:53:38 -0600

    Console application that asks for two numbers and divides them and then displays the result. Also contains a try catch statement with a few exceptions.

---
+++ C7P3/C7P3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C7P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // prompt user for dividen
+                Console.WriteLine("Enter Dividend:");
+                int dividend = int.Parse(Console.ReadLine());
+
+                // prompt user for divisor
+                Console.WriteLine("Enter Divisor:");
+                int divisor = int.Parse(Console.ReadLine());
+
+                // calculate the quotient
+                int quotient = dividend / divisor;
+
+                // display
+                Console.WriteLine($"{dividend} divided by {divisor} equals {quotient}");
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
