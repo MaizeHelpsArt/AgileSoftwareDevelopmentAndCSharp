a3a230fbaed938ce9afe1f7bebb6082f4a86137d
Jacob Huber <jacobnhuber@gmail.com>
2018-12-14 08:39:13 -0600

    Simple console application that was made using test driven development

---
+++ C7P4/C7P4/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C7P4
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+
+        public char GetLetterGrade(int score)
+        {
+            char letterGrade = 'F';
+            if (score >= 90)
+            {
+                letterGrade = 'A';
+            }
+            return letterGrade;
+        }
+    }
+}
---
+++ C7P4/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using C7P4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // create an object to test
+            Student s = new Student();
+
+            // define a test input and output value
+            int score = 90;
+            char expectedResult = 'A';
+
+            // run the method under test
+            char actualResult = s.GetLetterGrade(score);
+
+            // verify the result
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // create an object to test
+            Student s = new Student();
+
+            // define a test input and output value
+            for (int score = 90; score <= 100; score++)
+            {
+                char expectedResult = 'A';
+
+                // run method under test
+                char actualResult = s.GetLetterGrade(score);
+
+                // verify
+                Assert.AreEqual(expectedResult, actualResult);
+
+            }
+        }
+    }
+}
