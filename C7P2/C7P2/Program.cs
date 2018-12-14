b66ee20aa9b906e47e6485e51c24a0422e6d7aea
Jacob Huber <jacobnhuber@gmail.com>
2018-12-14 07:40:35 -0600

    Console application that will take a wholesale price and markup price and display the retail price. Exception handling with both.

---
+++ C7P2/C7P2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C7P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables to hold user values
+            double wholeSalePrice;
+            double markupPercentage;
+
+            // promt user for both
+            Console.WriteLine("Enter Wholesale Price:");
+            while (!(double.TryParse(Console.ReadLine(), out wholeSalePrice)) || (wholeSalePrice < 0 ))
+            {
+                Console.WriteLine("Wholsesale price must be numeric and positive.");
+            }
+
+            // wholesale price is valid
+            Console.WriteLine("Enter markup percentage");
+            while (!(double.TryParse(Console.ReadLine(), out markupPercentage)) || markupPercentage < 0)
+            {
+                Console.WriteLine("Markup must be numeric and positive");
+            }
+            // markup is valid
+            // calculate retail price
+            double retailPrice = wholeSalePrice * (1 + markupPercentage / 100);
+            // display
+            Console.WriteLine($"Retail Price {retailPrice}");
+            Console.ReadKey();
+        }
+    }
+}
